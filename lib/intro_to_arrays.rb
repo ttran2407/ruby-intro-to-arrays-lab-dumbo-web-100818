6494ab407a105c27b798dd24b45a404612b6cfbb
Tien Tran <tientran2407.vn@gmail.com>
2018-09-27 23:59:13 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,5 +3,10 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
-  
+  my_new_array = [1,2]
 end
+
+def indexing
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+end
+
