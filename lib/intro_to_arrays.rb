5a2742fcfad08920a99ea497913dbfebdaf3d7ee
Tien Tran <tientran2407.vn@gmail.com>
2018-09-28 00:06:35 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,13 +6,26 @@ def array_with_two_elements
   my_new_array = [1,2]
 end
 
-def indexing
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  first_element = taylor_swift[0]
-  third_element = taylor_swift[2]
-  last_element = taylor_swift[-1]
-  first_element_with_array_methods = taylor_swift.first
-  last_element_with_array_methods = taylor_swift.last
-  length_of_array = taylor_swift.size
+
+
+def first_element 
+  taylor_swift[0]
+end
+  
+def third_element 
+  taylor_swift[2]
+end
+  
+def last_element 
+  taylor_swift[-1]
+end
+def first_element_with_array_methods 
+  taylor_swift.first
+end
+def last_element_with_array_methods 
+  taylor_swift.last
+end
+def length_of_array 
+  taylor_swift.size
 end
 
